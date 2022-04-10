1335f6017883357b4201f4d1ee8397cd15fcddbb
ShrutiMore24 <97089262+ShrutiMore24@users.noreply.github.com>
2022-04-10 15:50:49 +0530

    Add files via upload

---
+++ april_9.js
@@ -0,0 +1,13 @@
+function show() {
+    var imgElement = document.getElementById('oneImage');
+    imgElement.height = 500;
+    imgElement.width = 500;
+
+}
+
+function reShow() {
+    var imgElement = document.getElementById('oneImage');
+    imgElement.height = 200;
+    imgElement.width = 200;
+
+}
